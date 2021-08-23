8d70cbcadb13fd359af702843ae23a7f46c2ce23
Silvris <58583688+Silvris@users.noreply.github.com>
2021-08-23 15:15:25 -0500

    create WavAddLoops, unfinished (also generic RIFF template based on AK RIFF)

---
+++ FINAL FANTASY PIXEL REMASTERS/FFPR_WavAddLoops.py
@@ -0,0 +1,19 @@
+import sys
+import os
+import struct
+
+class OtherChunk:
+    def __main__(self,tag,size,data):
+        self.tag = tag
+        self.size = size
+        self.data = data
+
+class SmplChunk:
+    def __main__(self,tag,size,zeros,loops):
+        self.tag = tag
+        self.size = size
+        self.zeros = zeros
+        self.loops = loops
+
+class WaveFile:
+    def __main__(self,tag,chunks)
