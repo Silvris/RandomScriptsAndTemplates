0f0272ada996cadf53fa215f0e1bd2d2c7fffe0c
Silvris <58583688+Silvris@users.noreply.github.com>
2021-10-20 00:03:44 -0500

    Update FFPR_GenerateSaveFilenames.py

--- FINAL FANTASY PIXEL REMASTERS/FFPR_GenerateSaveFilenames.py
+++ FINAL FANTASY PIXEL REMASTERS/FFPR_GenerateSaveFilenames.py
@@ -4,24 +4,26 @@ import sys
 def GenerateFilename(filename):
     print(filename)
     hashval = sha256(filename.encode('utf-8'))
-    print(hashval.digest())
+    #print(hashval.digest())
     encname = base64.b64encode(hashval.digest())
+    print(encname)
     return encname
 
 def GenerateFilenames():
     filenames = list()
     for i in range(99):
         #unknown if it actually goes to 99, easier to just prepare in case
+        #slot22 is quick save
         filenames.append("slot{}.sav".format(i))
-    filenames.append("system_slot.sav")#quicksave
-    filenames.append("PictureBook.sav")#backgrounds?
+    filenames.append("system_slot.sav")#autosave
+    filenames.append("PictureBook.sav")#backgrounds? bestiary?
     gennedFilenames = dict()
     for filename in filenames:
-        print(filename)
+        #print(filename)
         hashval = sha256(filename.encode('utf-8'))
-        print(hashval.digest())
+        #print(hashval.digest())
         encname = base64.b64encode(hashval.digest())
-        print(encname)
+        #print(encname)
         gennedFilenames[encname] = filename
 
     return gennedFilenames
