961f0f83560fd09803728b72018bb8cc3710524f
Silvris <58583688+Silvris@users.noreply.github.com>
2021-10-28 13:01:37 -0500

    PrettyPrint also unrolls json, update FF1 3DS MSG script

--- FINAL FANTASY PIXEL REMASTERS/PrettyPrintJson.py
+++ FINAL FANTASY PIXEL REMASTERS/PrettyPrintJson.py
@@ -8,10 +8,36 @@ dname = os.path.dirname(abspath)
 
 os.chdir(dname)
 
+def checkLowerJson(jobj):
+    for key in jobj:
+        #print(key)
+        #check for lower layers of json
+        if isinstance(jobj[key],str):
+            if len(jobj[key]) > 1:
+                #print(key)
+                #print(jobj[key][0])
+                if jobj[key][0] == '{':
+                    #we have a lower level of json
+                    newJson = json.loads(jobj[key])
+                    jobj[key] = checkLowerJson(newJson)
+        elif isinstance(jobj[key],list):
+            #check each item under the same settings
+            for i in range(len(jobj[key])):
+                if isinstance(jobj[key][i],str):
+                    if len(jobj[key][i]) > 1:
+                        #print(key)
+                        #print(jobj[key][0])
+                        if jobj[key][i][0] == '{':
+                            #we have a lower level of json
+                            newJson = json.loads(jobj[key][i])
+                            jobj[key][i] = checkLowerJson(newJson)
+    return jobj
+
 def prettyPrintJson(fileName):
     jf = open(fileName,'r',encoding='utf-8')
     jdata = json.load(jf)
     jf.close()
+    jdata = checkLowerJson(jdata)
     jf = open(fileName,'w',encoding='utf-8')
     json.dump(jdata,jf,indent=4,ensure_ascii=False)
     jf.close()
